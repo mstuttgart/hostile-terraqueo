411df031139658cdf56b26b3f7376a078878bed7
Michell Stuttgart <michellstut@gmail.com>
2021-02-06 09:44:12 -0300

    Add path2d and junk movement logic

--- HostileTerraqueo/Game.gd
+++ HostileTerraqueo/Game.gd
@@ -5,16 +5,26 @@ const SPEED = 50
 var screen_size
 var player_size
 
+var junk_list = [
+    preload("res://JunkSword.tscn"),
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    # Init random seed
+    randomize()
+
     # Get screen size
     screen_size = get_viewport_rect().size
 
     # Get player shape extents
     player_size =  $Player.get_node("CollisionShape2D").shape.get_extents()
 
+    # Set player initial position
     $Player.position = $InitialPosition.position
+
+    # Start timers
+    $JunkTimer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -44,3 +54,20 @@ func _process(delta):
     $Player.position.y = clamp($Player.position.y, player_size.y, screen_size.y - player_size.y)
 
 
+func _on_JunkTimer_timeout():
+    # Time to spawn junk in game scene
+
+    var idx = rand_range(0, junk_list.size() - 1)
+
+    # Instance junk
+    var junk = junk_list[idx].instance()
+
+    # Choose a random location on Path2D
+    $JunkPath/JunkSpawnPosition.offset = randi()
+
+    # Move the junk
+    junk.move($JunkPath/JunkSpawnPosition.position,
+              $JunkPath/JunkSpawnPosition.rotation)
+
+    # Add junk to scene
+    add_child(junk)
